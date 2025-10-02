500dc219afd0cce36ebc47dd3904623cfcdc9ffc
N4vv1 <sgrim420@gmail.com>
2025-10-02 09:44:57 +0800

    debug

--- index.php
+++ index.php
@@ -213,12 +213,7 @@ if (!$result) {
       </div>
     </section>
 
-   
-<h3>Image Path Test</h3>
-<ul>
-  <li>Logo: <img src="/icons/pawsig.png" alt="Logo test" width="80"></li>
-  <li>Home: <img src="/icons/home.png" alt="Home test" width="80"></li>
-</ul>
+  
 
   </main>
 
