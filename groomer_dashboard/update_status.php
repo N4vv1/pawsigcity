503c09b82f3cddada1808dbb73f715e0d440b78a
JB <johnbernardmitra25@gmail.com>
2025-11-05 00:29:07 +0800

    m

--- groomer_dashboard/update_status.php
+++ groomer_dashboard/update_status.php
@@ -18,12 +18,11 @@ $is_active = isset($input['is_active']) ? (bool)$input['is_active'] : false;
 // Update groomer status
 $query = "
     UPDATE groomer
-    SET is_active = $1::boolean, 
+    SET is_active = $1, 
         last_active = CURRENT_TIMESTAMP
     WHERE groomer_id = $2
 ";
 
-// Convert to PostgreSQL boolean - use true/false directly
 $result = pg_query_params($conn, $query, [$is_active ? 'true' : 'false', $groomer_id]);
 
 if ($result) {
