840d0dfd3748557b26c6b31916b7129426556a34
N4vv1 <sgrim420@gmail.com>
2025-11-15 13:38:50 +0800

    admin account archive

--- dashboard/manage_accounts/delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once '../../db.php';
-
-$id = intval($_GET['id']);
-pg_query($conn, "DELETE FROM users WHERE user_id = $id");
-header("Location: https://pawsigcity.onrender.com/dashboard/manage_accounts/accounts.php");
-exit;
-?>
